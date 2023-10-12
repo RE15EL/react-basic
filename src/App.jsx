46142ac5465247f7dffa4dccce03ac0bea9325e1
Reisel Valle <reiselvalle@gmail.com>
2023-10-12 16:12:21 -0500

    update btn styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import {MyButton} from './components/MyButton'
+import {MyButton} from './components/button/MyButton'
 
 function App() { 
 
   return (
-    <>
-      <header>
-        <div className="brand"> <h2> React Basic</h2> </div>
-        <nav className="navbar"></nav>
-        <div className="social"></div>
-      </header>
+    <>      
       <main>
         <MyButton caption="Hello World" />
       </main>
---
+++ src/components/header/MyHeader.jsx
@@ -0,0 +1,13 @@
+import './MyHeader.css';
+
+export const MyHeader = () => {
+  return (
+    <header className='my-header'>
+        <div className="brand"> <h2> React Basic</h2> </div>
+        <nav className="navbar"></nav>
+        <div className="social"></div>
+    </header>
+  )
+}
+
+export default MyHeader
