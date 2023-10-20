7f69de621fe678dddf7cd69b547d6b3d874a0cfb
Reisel Valle <reiselvalle@gmail.com>
2023-10-20 15:35:55 -0500

    binding fields from loginForm

--- src/components/forms/loginForm/LoginForm.jsx
+++ src/components/forms/loginForm/LoginForm.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 
 
 export function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (event) => setEmail(event.target.value);
+
+  const handlePassword = (event) => setPassword(event.target.value);
+
   return (
-    <div>LoginForm</div>
+    <form onSubmit={ (event) => {
+        event.preventDefault()
+        alert(`${email} - ${password}`)
+      } }>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={email} onChange={handleEmail}/>
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" value={password} onChange={handlePassword}/>
+      </div>
+      <button type="submit" >Submit</button>
+    </form>
   )
 }
